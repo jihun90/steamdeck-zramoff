bb8348a4f13a7f75bc1f45af163457e47ad6a254
jihun90 <jihun90.kim@icloud.com>
2024-06-21 13:54:57 +0900

    초기 커밋이자 마지막 커밋

---
+++ zr.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#sudo -i
+sudo systemctl stop dev-zram0.swap
+sudo systemctl disable dev-zram0.swap
+sudo swapoff /dev/zram0
+echo 1 > /sys/block/zram0/reset
+sudo modprobe -r zram
+sudo swapon -s
+
